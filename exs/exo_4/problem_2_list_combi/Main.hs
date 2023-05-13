d24b3f307f3f61f3dff9dcb6075c6fa884fa2045
0nyr <rascoussier.florian@gmail.com>
2023-05-13 11:04:02 +0200

    Added ex4, p2, b).

--- exs/exo_4/problem_2_list_combi/Main.hs
+++ exs/exo_4/problem_2_list_combi/Main.hs
@@ -1,4 +1,6 @@
 module Main where
+import Data.Char
+import Data.List
 
 -- a)
 sumAndLenCounter :: (Num a, Num b) => (a, b) -> a -> (a, b)
@@ -12,6 +14,18 @@ computeSumAndLength list =
     let (totalSum, len) = foldl sumAndLenCounter (0, 0) list
     in totalSum / len
 
+-- b)
+upLetterIfNeededAndLenCounter :: (Num a) => a -> Char -> (a, Char)
+upLetterIfNeededAndLenCounter counter charVal
+    | isLower charVal = (counter + 1, toUpper charVal)
+    | otherwise = (counter + 1, charVal)
+
+-- using mapAccumL :: (a->x->(a,y)) -> a -> [x] -> (a,[y])
+makeUpper :: String -> (Int, String)
+makeUpper listOfChar = 
+    (len, resList)
+    where (len, resList) = mapAccumL upLetterIfNeededAndLenCounter 0 listOfChar
+
 
 main :: IO ()
 main = do
@@ -20,3 +34,7 @@ main = do
     -- a)
     putStr "computeSumAndLength [1,2,3] (expect 2): "
     print (computeSumAndLength [1,2,3])
+
+    -- b)
+    putStr "makeUpper 'Hello world' (expect 2): "
+    print (makeUpper "Hello world")
